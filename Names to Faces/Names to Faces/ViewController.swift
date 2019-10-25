d8900f54e1e9ee461a7ab846ab96893160ff22fd
flin-7 <flin.7@icloud.com>
2019-10-24 17:28:35 -0700

    Day 42

---
+++ Names to Faces/Names to Faces/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Names to Faces
+//
+//  Created by Felix Lin on 10/24/19.
+//  Copyright © 2019 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        return cell
+    }
+}
+
