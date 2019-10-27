220b0ec895618fdd2df1e1ea45328c26e3591ecd
flin-7 <flin.7@icloud.com>
2019-10-27 16:19:29 -0700

    Day 45

---
+++ Pachinko/Pachinko/GameScene.swift
@@ -0,0 +1,48 @@
+//
+//  GameScene.swift
+//  Pachinko
+//
+//  Created by Felix Lin on 10/27/19.
+//  Copyright © 2019 Felix Lin. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+        background.scale(to: CGSize(width: frame.size.width, height: frame.size.height))
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 336, y: 0))
+        makeBouncer(at: CGPoint(x: 672, y: 0))
+        makeBouncer(at: CGPoint(x: 1008, y: 0))
+        makeBouncer(at: CGPoint(x: 1344, y: 0))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+    
+    func makeBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+}
