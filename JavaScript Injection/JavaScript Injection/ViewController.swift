9f6e9aa1849a4dfff5527c93460a62c867711d3b
flin-7 <flin.7@icloud.com>
2019-11-22 18:29:12 -0800

    Day 67

---
+++ JavaScript Injection/JavaScript Injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JavaScript Injection
+//
+//  Created by Felix Lin on 11/22/19.
+//  Copyright © 2019 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
